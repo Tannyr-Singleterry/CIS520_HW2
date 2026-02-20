e1e7b8e756a9e931d9264741f0998daf0bd73b3a
Samuel Green <Sjgreen@ksu.edu>
2026-02-19 22:41:02 -0600

    finished load_orocess_control_blocks function, no testing

--- hw2/src/process_scheduling.c
+++ hw2/src/process_scheduling.c
@@ -49,6 +49,7 @@ bool round_robin(dyn_array_t *ready_queue, ScheduleResult_t *result, size_t quan
 
 dyn_array_t *load_process_control_blocks(const char *input_file) 
 {
+	//checks for valid input file
 	if(input_file == NULL)
 		return NULL;
 	char* badChars = "\n\t\r\v\f";
@@ -62,9 +63,50 @@ dyn_array_t *load_process_control_blocks(const char *input_file)
 	}
 
 	FILE* file = fopen(input_file, "rb");
-	if(!file)
+	if(file == NULL)
 		return NULL;
-	
+
+	//reads the first element of the file to see the size
+	uint32_t* elements; //data type might need to be "size_t" but idk
+	if(!fread(elements, 4, 1, file))
+		return NULL;
+	//checks that there are elements to read
+	if(elements == 0)
+		return NULL
+
+	//array to hold read PCBs
+	dyn_array_t* PCBs = dyn_array_create(elements, sizeof(ProcessControlBlock_t), NULL);
+	if(PCBs == NULL)
+		return NULL;
+
+	int ctr; //counter to keep track of what we are reading ie burst time, priority, or arrival 
+	ProcessControlBlock_t* currentPCB;
+	for(uint32_t i = 0; i < elements*3; i++)
+	{
+		uint32_t info; //stores the info read
+		if(!fread(&info, 4, 1, file))
+			return NULL;
+		//checks where to which info is read and creates a new PCB when needed
+		switch(ctr)
+		{
+			case 0:
+				current = malloc(sizeof(ProcessControlBlock_t));
+				curent->remaining_burst_time = info;
+				ctr++;
+				break;
+			case 1:
+				current->priority = info;
+				ctr++;
+				break;
+			case 2:
+				current->arrival = info;
+				if(!dyn_array_push_fron(PCBs, current))
+					return NULL;
+				ctr = 0;
+				break;
+		}
+	}
+	return PCBs;
 }
 
 bool shortest_remaining_time_first(dyn_array_t *ready_queue, ScheduleResult_t *result) 
